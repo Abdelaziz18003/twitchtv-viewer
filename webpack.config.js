4109f073de96e2decacbaa711894eae1d5579360
Abdelaziz18003 <m.abdelaziz1993@gmail.com>
2016-09-08 21:56:36 +0100

    setup working environement

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var webpack = require("webpack");
+var path = require("path");
+
+module.exports = {
+    entry: "./src/jsx/app.jsx",
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                query: {
+                    presets: ["react", "es2015"]
+                }
+            }
+        ]
+    },
+    output: {
+        path: dirname + "/dist/js/",
+        filename: "app.js"
+    }
+}
